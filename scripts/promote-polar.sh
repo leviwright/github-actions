aa416cb97ac06cdd5376429048f64129101ba577
leviwright <levi.wright@lumio.com>
2023-03-22 16:12:27 -0600

    attempt to read polar file from bash script

--- scripts/promote-polar.sh
+++ scripts/promote-polar.sh
@@ -26,14 +26,14 @@ sourceFile="./policies/environments/development.polar" #do some interpolation he
 isPastCommentSection=false
 while IFS= read -r line
 do
-  if [ isPastCommentSection == true ]
+  if [ isPastCommentSection = true ]
   then 
     echo $line
     echo $line >> "./policies/environments/test.polar"
   fi
-  if [ $line == "# -- End Comment Section --" ]
+  if [ $line = "# -- End Comment Section --" ]
   then 
-    isPastCommentSection = true
+    isPastCommentSection=true
   fi
 done < "$sourceFile"
 
