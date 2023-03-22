4de1c13cb33770bc3c19a0e738046371d56d4417
leviwright <levi.wright@lumio.com>
2023-03-22 15:08:09 -0600

    attempt to read polar file from bash script

--- scripts/promote-polar.sh
+++ scripts/promote-polar.sh
@@ -40,6 +40,8 @@ do
   ((index++))
 done < "$input"
 
+echo $line "FINISHED"
+
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/runner/.bash_profile
