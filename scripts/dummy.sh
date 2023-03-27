7b11153d90567c570f4ca2976334b70b49a1058d
leviwright <levi.wright@lumio.com>
2023-03-27 16:12:24 -0600

    another one.

--- scripts/dummy.sh
+++ scripts/dummy.sh
@@ -1,10 +1,1 @@
-#sed -i '' '/whatever/' dummy.polar
-
-lineToStart="`grep -n '# -- End Comment Section --' dummy.polar | cut -d: -f 1`"
-((lineToStart++))
-
-echo $lineToStart
-echo "${lineToStart},\$d"
-
-sed -i '' "${lineToStart},\$d" dummy.polar
-
+echo $'This is First Line \nThis is Second Line'
