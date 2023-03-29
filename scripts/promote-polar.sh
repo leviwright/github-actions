9ad87d23940a6b7d4521c47ecbef3ece7285aeb7
leviwright <levi.wright@lumio.com>
2023-03-29 16:02:18 -0600

    update.

--- scripts/promote-polar.sh
+++ scripts/promote-polar.sh
@@ -58,7 +58,7 @@ git status
 priorCommitMessage=$(git whatchanged -n 1 --format=%b -- policies/environments/${sourceEnv}.polar)
 formattedPriorCommitMessage=${priorCommitMessage%$'\n'*}
 
-git commit -m "${formattedPriorCommitMessage}" 
+git commit -m "Promoting polar file contents from ${sourceEnv} to ${targetEnv}" 
 
 echo "Pushing changes to remote..."
 git push origin $branchName 
