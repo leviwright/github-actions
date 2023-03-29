01f9110be5fc7e389fa599b4f92ae4f983e77158
leviwright <levi.wright@lumio.com>
2023-03-29 00:21:10 -0600

    a thing to see a commit message.

--- scripts/promote-polar.sh
+++ scripts/promote-polar.sh
@@ -31,13 +31,6 @@ fi
 
 echo "Attempting run to promote to:" $targetEnv
 echo "Run triggered by github user:" $actor
-echo "Installing github cli in order to provide easy hook for creating a pull request..."
-# type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
-# curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-# && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-# && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-# && sudo apt update \
-# && sudo apt install gh -y
 
 echo "Ensure we are starting with the latest changes on the main branch..."
 git checkout main 
